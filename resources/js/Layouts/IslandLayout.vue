180512eb902bffedc44bd332f24e4795a8051f69
Akasiek <kpomykala2002@gmail.com>
2024-03-02 23:41:50 +0100

    style: Login pages

--- resources/js/Layouts/IslandLayout.vue
+++ resources/js/Layouts/IslandLayout.vue
@@ -5,10 +5,10 @@ import WrapperLayout from "@/Layouts/WrapperLayout.vue";
 <template>
   <WrapperLayout>
     <div
-      class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900"
+      class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-zinc-950"
     >
       <div
-        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-zinc-950 border dark:border-zinc-800 shadow-md overflow-hidden sm:rounded-lg"
       >
         <slot />
       </div>
